4331ca978c0678e779b3d1a2bbfd0c3b118c5d1b
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-06-15 16:45:35 +0300

    Доробив додаткове завдання вибачаюся після занять у той вівторок був у дорозі майже весь час

---
+++ js/index8.js
@@ -0,0 +1,12 @@
+// 1.Коля
+// 2.Правильно
+// 3.Неправильно
+// 4.
+// 'use strict'
+// let showMessage;
+// if (2 > 1) {
+//     showMessage = function () {
+//         console.log('mesage');
+//     };
+// };
+// showMessage();
